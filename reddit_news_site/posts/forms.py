f825172b6a0236c65cd475b1007888b10d2847ce
Brendan <brendancollins89@hotmail.com>
2024-11-13 22:07:49 +0000

    Add forms for Post and Comment models

---
+++ reddit_news_site/posts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Post, Comment
+
+# Form for creating or updating posts
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'category']
+
+# Form for creating or updating comments
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
