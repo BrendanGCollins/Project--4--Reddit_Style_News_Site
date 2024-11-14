8e0491517df4094cf3b9dd23dbc99d1c328d6324
Brendan <brendancollins89@hotmail.com>
2024-11-14 23:35:17 +0000

    Add header styling with boothstrap

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Comment, Category
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Display list of all posts
 def post_list(request):
@@ -27,14 +28,20 @@ def post_detail(request, post_id):
     return render(request, 'posts/post_detail.html', {'post': post, 'form': form, 'comments': comments})
 
 # Create a new post
-@login_required
+@login_required(login_url='login')
 def create_post(request):
+    # Add an informational message if the user is not authenticated and they try to access this page
+    if not request.user.is_authenticated:
+        messages.info(request, "You need to be logged in to create a post. Please log in or register.")
+
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
+            # Add a success message after creating the post
+            messages.success(request, "Your post has been created successfully!")
             return redirect('home')
     else:
         form = PostForm()
@@ -42,17 +49,20 @@ def create_post(request):
     return render(request, 'posts/create_post.html', {'form': form})
 
 # Update an existing post
-@login_required
+@login_required(login_url='login')
 def update_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
 
+    # Ensure that only the author can edit their post
     if request.user != post.author:
+        messages.error(request, "You do not have permission to edit this post.")
         return redirect('post_detail', post_id=post.id)
 
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             form.save()
+            messages.success(request, "Your post has been updated successfully.")
             return redirect('post_detail', post_id=post.id)
     else:
         form = PostForm(instance=post)
@@ -60,15 +70,18 @@ def update_post(request, post_id):
     return render(request, 'posts/update_post.html', {'form': form, 'post': post})
 
 # Delete an existing post
-@login_required
+@login_required(login_url='login')
 def delete_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
 
+    # Ensure that only the author can delete their post
     if request.user != post.author:
+        messages.error(request, "You do not have permission to delete this post.")
         return redirect('post_detail', post_id=post.id)
 
     if request.method == 'POST':
         post.delete()
+        messages.success(request, "The post has been deleted successfully.")
         return redirect('home')
 
     return render(request, 'posts/delete_post.html', {'post': post})
