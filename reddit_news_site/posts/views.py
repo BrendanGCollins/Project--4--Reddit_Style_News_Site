7efe8c9d9a49b0521957e1b52fe03047701c059c
Brendan <brendancollins89@hotmail.com>
2024-11-14 00:25:06 +0000

    Add views for creating, updating, and deleting posts

--- reddit_news_site/posts/views.py
+++ reddit_news_site/posts/views.py
@@ -25,3 +25,50 @@ def post_detail(request, post_id):
         form = CommentForm()
 
     return render(request, 'posts/post_detail.html', {'post': post, 'form': form, 'comments': comments})
+
+# Create a new post
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+
+    return render(request, 'posts/create_post.html', {'form': form})
+
+# Update an existing post
+@login_required
+def update_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.user != post.author:
+        return redirect('post_detail', post_id=post.id)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'posts/update_post.html', {'form': form, 'post': post})
+
+# Delete an existing post
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.user != post.author:
+        return redirect('post_detail', post_id=post.id)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+
+    return render(request, 'posts/delete_post.html', {'post': post})
