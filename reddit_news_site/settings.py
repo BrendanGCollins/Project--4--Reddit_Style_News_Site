4b26614c5c9dd5a30b2e13d3f465d0a36f54891d
Brendan <brendancollins89@hotmail.com>
2024-11-14 16:34:37 +0000

    Add base template to settings.py

--- reddit_news_site/settings.py
+++ reddit_news_site/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'reddit_news_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Add template directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
